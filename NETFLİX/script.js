83ef64446111dd85a127045bf6e2e95843c17399
celil çavuş <celilcavus13@hotmail.com>
2023-05-25 19:25:05 +0300

    Inputlar duzeltildi SSS Tamamlandı

---
+++ NETFLİX/script.js
@@ -0,0 +1,45 @@
+var click1 = document.getElementById('sss1');
+var des1 = document.getElementById("des1");
+click1.addEventListener('click',()=>{
+    des1.classList.add('display-block');
+});
+
+var click1 = document.getElementById('sss2');
+var des1 = document.getElementById("des2");
+click1.addEventListener('click',()=>{
+    des1.classList.add('display-block');
+});
+
+
+var click1 = document.getElementById('sss3');
+var des1 = document.getElementById("des3");
+click1.addEventListener('click',()=>{
+    des1.classList.toggle('display-block');
+});
+
+
+
+var click1 = document.getElementById('sss4');
+var des1 = document.getElementById("des4");
+click1.addEventListener('click',()=>{
+    des1.classList.toggle('display-block');
+});
+
+
+var click1 = document.getElementById('sss5');
+var des1 = document.getElementById("des5");
+click1.addEventListener('click',()=>{
+    des1.classList.toggle('display-block');
+});
+
+
+var click1 = document.getElementById('sss6');
+var des1 = document.getElementById("des6");
+click1.addEventListener('click',()=>{
+    des1.classList.toggle('display-block');
+});
+
+
+
+
+
