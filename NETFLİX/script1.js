c9ce070f84a2ecbe24005da1bca5cd07ae058758
Naz <72349691+Nazzergun@users.noreply.github.com>
2023-06-06 00:42:49 +0300

    first commit

---
+++ NETFLİX/script1.js
@@ -0,0 +1,17 @@
+const arrows = doucment.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  let clickCounter = 0;
+  const imageItem = movieLists[i].querySelectorAll("img").length;
+  arrow.addEventListener("click", function () {
+    clickCounter++;
+    if (imageItem - (4 + clickCounter) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+    }
+  });
+});
