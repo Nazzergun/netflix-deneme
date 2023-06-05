13533c94ccd3f104edaf45abceb31e740cecdb8f
Naz <72349691+Nazzergun@users.noreply.github.com>
2023-06-06 01:02:30 +0300

    first commit

--- NETFLİX/script1.js
+++ NETFLİX/script1.js
@@ -15,3 +15,12 @@ arrows.forEach((arrow, i) => {
     }
   });
 });
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelector(
+  ".container,.navbar,.sidebar,.sidebar i,.toggle,.toggle-ball,.movie-list-filter select,.movie-list-title"
+);
+
+ball.addEventListener("click", function () {
+  items.forEach((item) => item.classList.toggle("active"));
+});
